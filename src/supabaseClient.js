44c9519e18162de09db185793202675086471de9
tyagikartik5 <tyagikartik576@gmail.com>
2026-02-14 10:29:55 +0530

    feat: use credential fallbacks for easier deployment and remove .env

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-    console.warn('Supabase credentials missing. Check your .env file.');
-}
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://gvjgbzcxmxrlmsbdsmus.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_NOXMlW0_adxGVLzziVGRKg_63YoqB-2';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
